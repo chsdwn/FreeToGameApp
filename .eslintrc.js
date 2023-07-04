d898e64b5f665bb5b4c002bc75aca34bb6e74dd5
chsdwn <chasedown@hotmail.com>
2023-07-04 23:28:43 +0300

    feat(eslint): add type and Text import rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,15 @@ module.exports = {
             message: 'Import pre-configured axios from @/lib',
           },
         ],
+        paths: [
+          {
+            name: 'react-native',
+            importNames: ['Text'],
+            message: 'Import Text from react-native-paper',
+          },
+        ],
       },
     ],
+    '@typescript-eslint/consistent-type-imports': 'error',
   },
 };
