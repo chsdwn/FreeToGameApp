c182cacea838d5077c2b39ef2b466f9da36dc406
chsdwn <chasedown@hotmail.com>
2023-07-05 14:24:44 +0300

    fix(GamesListHeader): calculate badge content dynamically

--- src/features/games/components/GamesListHeader.tsx
+++ src/features/games/components/GamesListHeader.tsx
@@ -2,17 +2,29 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Appbar, Badge } from 'react-native-paper';
 
-export const GamesListHeader = () => (
-  <Appbar.Header>
-    <Appbar.Content title="Free Games" titleStyle={styles.title} />
-    <View>
-      <Badge visible size={16} style={styles.badge}>
-        3
-      </Badge>
-      <Appbar.Action icon="filter-variant" onPress={() => {}} />
-    </View>
-  </Appbar.Header>
-);
+import { useGamesFilterModalStore, useGamesFilterStore } from '../store';
+
+export const GamesListHeader = () => {
+  const { platform, category, sortBy } = useGamesFilterStore();
+  const { show } = useGamesFilterModalStore();
+
+  let badgeCount = 0;
+  if (platform !== 'all') badgeCount++;
+  if (category) badgeCount++;
+  if (sortBy) badgeCount++;
+
+  return (
+    <Appbar.Header statusBarHeight={0}>
+      <Appbar.Content title="Free Games" titleStyle={styles.title} />
+      <View>
+        <Badge visible={!!badgeCount} size={16} style={styles.badge}>
+          {badgeCount}
+        </Badge>
+        <Appbar.Action icon="filter-variant" onPress={show} />
+      </View>
+    </Appbar.Header>
+  );
+};
 
 const styles = StyleSheet.create({
   title: {
