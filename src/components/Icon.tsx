cd95a106d2299ba234d880448d3fab02979abb09
chsdwn <chasedown@hotmail.com>
2023-07-05 15:51:11 +0300

    feat(components): add icon component

---
+++ src/components/Icon.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-restricted-imports */
+import React from 'react';
+import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import type { IconProps } from 'react-native-vector-icons/Icon';
+
+export const Icon = (props: IconProps) => {
+  return <MCIcon {...props} />;
+};
