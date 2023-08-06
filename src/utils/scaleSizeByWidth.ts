d368c2cb4ba09bdd8efb0b37b9de7fbd2590b281
chsdwn <chasedown@hotmail.com>
2023-08-06 22:18:32 +0300

    fix(scaleSizeByWidth): remove screen orientation check

--- src/utils/scaleSizeByWidth.ts
+++ src/utils/scaleSizeByWidth.ts
@@ -4,12 +4,10 @@ import { widthPercentageToDP } from 'react-native-responsive-screen';
 import { breakpoints } from '@/config/theme';
 
 export const scaleSizeByWidth = (size: number) => {
-  const { width, height } = Dimensions.get('screen');
+  const { width } = Dimensions.get('screen');
   let divisor = 1;
-  if (height > width) {
-    if (width >= breakpoints.md) divisor = 3;
-    else if (width >= breakpoints.sm) divisor = 2;
-  }
+  if (width >= breakpoints.sm) divisor = 2;
+  if (width >= breakpoints.md) divisor = 3;
 
   return PixelRatio.roundToNearestPixel(widthPercentageToDP(size) / divisor);
 };
