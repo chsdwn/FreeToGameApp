150b410297ce7cc9a4172b08182e0bf6de5d14c9
chsdwn <chasedown@hotmail.com>
2023-08-12 13:36:57 +0300

    feat(components): add Chip component

---
+++ src/components/Chip.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Chip as PaperChip, Text } from 'react-native-paper';
+
+import { useStyle, useTheme } from '@/hooks';
+
+type IProps = {
+  label: string;
+  active?: boolean;
+  onPress?: () => void;
+};
+export const Chip = ({ label, active = false, onPress }: IProps) => {
+  const theme = useTheme();
+
+  return (
+    <PaperChip
+      mode={active ? 'flat' : 'outlined'}
+      onPress={onPress}
+      style={useStyle(
+        () => [styles.chip, { borderColor: theme.colors.tertiary }],
+        [theme.colors.tertiary],
+      )}
+    >
+      <Text variant="labelMedium">{label}</Text>
+    </PaperChip>
+  );
+};
+
+const styles = StyleSheet.create({
+  chip: {
+    borderWidth: 1,
+  },
+});
--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable no-restricted-imports */
 import React from 'react';
 import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
-
-import type { IconProps } from 'react-native-vector-icons/Icon';
+import { IconProps } from 'react-native-vector-icons/Icon';
 
 export const Icon = (props: IconProps) => {
   return <MCIcon {...props} />;
