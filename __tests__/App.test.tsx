1232656cf2794dea18e662460e1dc98bc1166a46
chsdwn <chasedown@hotmail.com>
2023-08-14 16:19:38 +0300

    test: configure jest settings

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,14 +1,10 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import { App } from '../src/App';
+import { render } from '@testing-library/react-native';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('[App]', () => {
+  it('should render correctly', () => {
+    render(<App />);
+  });
 });
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  preset: 'react-native',
+  preset: 'jest-expo',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/types/*',
+    '!**/coverage/**',
+    '!**/node_modules/**',
+    '!**/babel.config.js',
+    '!**/jest.setup.js',
+  ],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+    '<rootDir>/jest/setup-after-env.js',
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|react-native-vector-icons)',
+  ],
 };
