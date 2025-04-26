113380ed2758a7c1431029b1f7b3ced13a4dd7e8
chsdwn <chasedown@hotmail.com>
2025-04-26 11:41:01 +0300

    build: fix storybook 'Unable to resolve' issue

--- metro.config.js
+++ metro.config.js
@@ -3,13 +3,13 @@ const withStorybook = require('@storybook/react-native/metro/withStorybook');
 const { getDefaultConfig } = require('expo/metro-config');
 const path = require('node:path');
 
-if (process.env.STORYBOOK_ENABLED === 'true') {
-  /** @type {import("expo/metro-config").MetroConfig} */
-  const config = getDefaultConfig(__dirname);
-  module.exports = withStorybook(config, {
-    enabled: true,
-    configPath: path.resolve(__dirname, './.storybook'),
-  });
-} else {
-  module.exports = getSentryExpoConfig(__dirname);
-}
+module.exports = getSentryExpoConfig(__dirname, {
+  getDefaultConfig: (projectRoot, options) => {
+    const config = getDefaultConfig(projectRoot, options);
+
+    return withStorybook(config, {
+      enabled: true,
+      configPath: path.resolve(__dirname, './.storybook'),
+    });
+  },
+});
